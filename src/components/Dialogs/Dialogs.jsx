31b5ad3aea442aab5037aaa292722528b2c10c14
MaxorMaximus <romanchuk.max222@gmail.com>
2021-01-31 23:05:43 +0300

    commit

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
-
-
-const DialogItem = (props) => {
-
-    let path = "/dialogs/" + props.id;
-
-    return <div className={s.dialog + ' ' + s.active}>
-        <NavLink to={path}>{props.name}</NavLink>
-    </div>
-}
-const Message = (props) => {
-    return (
-        <div className={s.dialog}>{props.message}</div>
-    )
-}
-
+import DialogItem from './DialogItem/DialogItem' ;
+import Message from './Message/Message' ;
 
 const Dialogs = (props) => {
 
@@ -43,21 +28,13 @@ let messagesElements =messagesData.map(message => <Message message={message.mess
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
                 {dialogsElements}
-                {/* <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} /> */}
             </div>
             <div className={s.messages}>
                 {messagesElements}
-                {/* <Message message={messagesData[0].message} id={messagesData[0].id} />
-                <Message message={messagesData[1].message} id={messagesData[0].id} />
-                <Message message={messagesData[2].message} id={messagesData[0].id} /> */}
             </div>
         </div>
     )
 }
 
 export default Dialogs;
+
